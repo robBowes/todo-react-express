3de923ac80de4d531a21a0bfb15dc0f016fa7001
decodejacques <31515171+decodejacques@users.noreply.github.com>
2018-05-01 22:18:42 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,6 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.raw({ type: '*/*' }))
 
-
-// The following two endpoints are so that the browser can load the HTML and Javascript
-app.get('/', (req, res) => res.send(fs.readFileSync('./public/index.html').toString()))
-app.get('/app.js', (req, res) => res.send(fs.readFileSync('./public/app.js').toString()))
-
-// 
 let serverState = {
     items: []
 }
